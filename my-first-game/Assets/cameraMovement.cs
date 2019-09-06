19314a1f494043c99a5f101e37c764cbfc40b2ac
arman <riasiarman@yahoo.com>
2019-09-07 01:59:11 +0430

    Adding some features to camera movement

--- my-first-game/Assets/cameraMovement.cs
+++ my-first-game/Assets/cameraMovement.cs
@@ -4,9 +4,19 @@ public class cameraMovement : MonoBehaviour
 {
     public Transform player;
     public Vector3 offset;
-
+    public Vector3 secondOffset;
+    public char key = 'b';
     void Update()
     {
+
+      if (Input.GetKey("b") || key == 'b'){
+                key = 'b';
         transform.position = player.position + offset;
+      }
+      if (Input.GetKey("v") || key == 'v'){
+        key = 'v';
+          transform.position = player.position + secondOffset;
+      }
+
     }
 }
