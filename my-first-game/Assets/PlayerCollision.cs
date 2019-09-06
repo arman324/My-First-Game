be8d12e46808ac5d65a405e3cbcef23b6ff4d55e
arman <riasiarman@yahoo.com>
2019-09-07 01:41:06 +0430

    Design Player collision

---
+++ my-first-game/Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+  public PlayerMovement movement;
+  public Rigidbody rb;
+
+    void OnCollisionEnter (Collision CollisonInfo){
+      if (CollisonInfo.collider.tag == "stone"){
+          movement.enabled = false;
+          rb.useGravity = true;
+      }
+    }
+}
